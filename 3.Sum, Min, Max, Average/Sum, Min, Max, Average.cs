c9fcd9b1206b6d18d0e22e844a4944ed0372f602
splafi <martin_petkov555@abv.bg>
2016-10-22 17:26:23 +0300

    Dictonaries_Excersise

---
+++ 3.Sum, Min, Max, Average/Sum, Min, Max, Average.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Sum__Min__Max__Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            double[] nums = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                nums[i] = double.Parse(Console.ReadLine());
+            }
+            var sum = nums.Sum();
+            var min = nums.Min();
+            var max = nums.Max();
+            var avarege = nums.Average();
+            Console.WriteLine($"Sum = {sum}");
+            Console.WriteLine($"Min = {min}");
+            Console.WriteLine($"Max = {max}");
+            Console.WriteLine($"Average = {avarege}");
+        }
+    }
+}
---
+++ 5.Short Words Sorted/Short Words Sorted.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrayOfWords = Console.ReadLine()
+                .ToLower()
+                .Split(new char[] { '.', ',', ':', ';', '(', ')', '[', ']', '"', '\'', '\\', '/', '!', '?', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            var smallWords = new List<string>();
+
+            foreach (var word in arrayOfWords)
+            {
+                if (word.Length < 5)
+                {
+                    smallWords.Add(word);
+                }
+            }
+            var result = smallWords
+                .OrderBy(w => w)
+                .Distinct();
+
+            Console.WriteLine(string.Join(", ", result));
+
+        }
+    }
+}
